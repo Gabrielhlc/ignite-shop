662442d295bc63a8fb24bd45425c2ef2c92e2ddf
Gabriel Carvalho <gabrielhenlc@gmail.com>
2022-11-14 16:28:03 -0300

    adding nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,14 @@ import { globalStyles } from '../styles/global'
 import { Container } from '../styles/pages/app';
 import { CartContextProvider } from '../contexts/cartContext';
 import Header from '../components/header';
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import Head from 'next/head'
+
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 globalStyles();
 
@@ -11,6 +19,9 @@ globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CartContextProvider>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
       <Container>
         <Header />
         <Component {...pageProps} />
